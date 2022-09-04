40f9fe853fd702c60aa8459c5d8b762e7303d92f
Akum65358Blaise <akumacha@gmail.comgit config --global user.email akumacha@gmail.comgit config --global user.email akumacha@gmail.com>
2022-09-04 11:10:31 +0100

    custom MVC framework using OOP

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Define DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "phpzo");
+
+// Define URL
+define("ROOT_PATH", "/phpzo/");
+define("ROOT_URL", "http://localhost/phpzo/");
